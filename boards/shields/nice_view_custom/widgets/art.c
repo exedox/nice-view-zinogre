68ae6aca0ac8c2c108c66df5b7e70f8c075238c4
exedox <55308770+exedox@users.noreply.github.com>
2025-04-21 01:07:54 -0700

    Update art.c

--- boards/shields/nice_view_custom/widgets/art.c
+++ boards/shields/nice_view_custom/widgets/art.c
@@ -312,6 +312,11 @@ const lv_img_dsc_t zynogre = {
   .data = zynogre_map,
 };
 
+
+#ifndef LV_ATTRIBUTE_IMG_IG
+#define LV_ATTRIBUTE_IMG_IG
+#endif
+
 const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_IG uint8_t ig_map[] = {
 #if CONFIG_NICE_VIEW_WIDGET_INVERTED
         0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
