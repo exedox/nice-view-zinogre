459fd3df99d4e3bb47116c451a2200f0e0475bcc
exedox <55308770+exedox@users.noreply.github.com>
2026-06-23 19:10:41 -0700

    Update art.c

--- boards/shields/nice_view_custom/widgets/art.c
+++ boards/shields/nice_view_custom/widgets/art.c
@@ -95,7 +95,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_ZYNOGRE u
 };
 
 const lv_img_dsc_t zynogre = {
-  .header.cf = LLV_COLOR_FORMAT_I1,
+  .header.cf = LV_COLOR_FORMAT_I1,
 
 
   .header.w = 139,
